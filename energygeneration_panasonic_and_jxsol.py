d528bc61d1a5388f69bc5997b1691c32f0803dac
nicolekuek <46975436+nicolekuek@users.noreply.github.com>
2019-05-02 22:57:50 +0100

    Removed unnecessary comments

--- energygeneration_panasonic_and_jxsol.py
+++ energygeneration_panasonic_and_jxsol.py
@@ -25,13 +25,13 @@ numberPVs_JXSOL = 6184
 
 'Importing Irradiance Data and Calculating Energy Generation'
 
-areacovered_panasonic = areaPV_panasonic*numberPVs_panasonic #m^2 (area of parking spaces covered, 12000m^2 maximum)
+areacovered_panasonic = areaPV_panasonic*numberPVs_panasonic #m^2
 areacovered_JXSOL = areaPV_JXSOL*numberPVs_JXSOL # m^2
 
 'Using averaged data from 2010 to 2016 at 21.8 degree slope'
 df1 = pd.read_csv('PVGIS_22deg_-5deg_2007_2016.csv', usecols=[9])
 
-energygen_panasonic = efficiencyPV_panasonic*areacovered_panasonic*df1.values #kWh (W to kW conversion already done in Excel)
+energygen_panasonic = efficiencyPV_panasonic*areacovered_panasonic*df1.values #kWh
 energygen_JXSOL = efficiencyPV_JXSOL*areacovered_JXSOL*df1.values
 
 'Plotting the energy generation profiles'
